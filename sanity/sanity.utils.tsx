57bbeae33fedc05f00887bf96e1d4cc2ac93c2cf
jorlar <jqern.larsen@gmail.com>
2024-11-14 22:52:10 +0100

    :zap: Make projects clickable

--- sanity/sanity.utils.tsx
+++ sanity/sanity.utils.tsx
@@ -15,10 +15,33 @@ export async function getProjects(): Promise<Project[]> {
         _id,
         _createdAt,
         name, 
-        "slug": clug.current,
+        "slug": slug.current,
         "image": image.asset->url,
         url,
         content
         }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: "hwqw748q",
+
+    dataset: "satch_data",
+
+    apiVersion: "2024-11-14",
+  });
+
+  return client.fetch(
+    groq`*[_type== "project" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        name, 
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+        }`,
+    { slug }
   );
 }
