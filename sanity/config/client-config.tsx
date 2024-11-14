7b3b883a55fcf18ad21c775fb6f35c55fcd053c6
jorlar <jqern.larsen@gmail.com>
2024-11-14 22:57:32 +0100

    :bug: Clean-up code

---
+++ sanity/config/client-config.tsx
@@ -0,0 +1,9 @@
+const config = {
+  projectId: "hwqw748q",
+
+  dataset: "satch_data",
+
+  apiVersion: "2024-11-14",
+};
+
+export default config;
--- sanity/sanity.utils.tsx
+++ sanity/sanity.utils.tsx
@@ -1,5 +1,6 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
+import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
   const client = createClient({
@@ -10,7 +11,7 @@ export async function getProjects(): Promise<Project[]> {
     apiVersion: "2024-11-14",
   });
 
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type== "project"]{
         _id,
         _createdAt,
@@ -24,15 +25,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  const client = createClient({
-    projectId: "hwqw748q",
-
-    dataset: "satch_data",
-
-    apiVersion: "2024-11-14",
-  });
-
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type== "project" && slug.current == $slug][0]{
         _id,
         _createdAt,
