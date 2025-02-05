cf39fb842d634687ba3ef736fc70e096fb1595e5
jorlar <jqern.larsen@gmail.com>
2025-02-05 14:24:56 +0100

    :art: Fix header-image style and add products

--- sanity/schemas/index.tsx
+++ sanity/schemas/index.tsx
@@ -1,7 +1,6 @@
 import page from "./page-schema";
 import project from "./project-schema";
-import product from "./product-schema";
 
-const schemas = [product, project, page];
+const schemas = [project, page];
 
 export default schemas;
